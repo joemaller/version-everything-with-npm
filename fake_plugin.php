1a5619eb2533093b465e191f0df4460191862c0b
Joe Maller <joe@ideasonpurpose.com>
2016-10-08 23:06:03 -0400

    5.2.6

--- fake_plugin.php
+++ fake_plugin.php
@@ -3,7 +3,7 @@ Plugin Name: Fake Version Test Plugin
 Plugin URI: http://www.ideasonpurpose.com
 Description: lorem ipsum
 Author: Ideas On Purpose
-Version: 4.2.2
+Version: 5.2.6
 Author URI: http://www.github.com/joemaller
 */
 
