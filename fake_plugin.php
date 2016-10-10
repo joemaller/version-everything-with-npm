54d700dbcbce45530be14b742f79661cdace7ce0
Joe Maller <joe@ideasonpurpose.com>
2016-10-10 12:59:05 -0400

    5.5.2-0

--- fake_plugin.php
+++ fake_plugin.php
@@ -3,6 +3,6 @@ Plugin Name: Secret Internal Plugin
 Plugin URI: http://www.ideasonpurpose.com
 Description: An example plugin Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi libero voluptas fugit.
 Author: Joe Maller
-Version: 5.5.1
+Version: 5.5.2-0
 Author URI: http://www.github.com/joemaller
 */
