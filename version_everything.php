feedc1155e89b730cf3cea1a51092e6aba9f0e12
Joe Maller <joe@ideasonpurpose.com>
2016-10-11 13:38:44 -0400

    1.0.1

--- version_everything.php
+++ version_everything.php
@@ -3,6 +3,6 @@ Plugin Name: Version Everything
 Plugin URI: http://www.joemaller.com/
 Description: Version everything with npm.
 Author: Joe Maller
-Version: 5.6.2
+Version: 1.0.1
 Author URI: http://www.github.com/joemaller
 */
