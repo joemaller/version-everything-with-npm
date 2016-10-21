8f20e4abf321dcfea8f1370e696fbf707a12780b
Joe Maller <joe@ideasonpurpose.com>
2016-10-21 17:11:09 -0400

    example files and gitignore

---
+++ example_wordpress_plugin.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Version Everything
+Plugin URI: https://github.com/joemaller/version-everything-with-npm
+Description: Version everything with npm: Example WordPress plugin
+Author: Joe Maller
+Version: 1.0.1-2
+Author URI: http://www.github.com/joemaller
+*/
--- version_everything.php
+++
@@ -1,8 +0,0 @@
-/*
-Plugin Name: Version Everything
-Plugin URI: http://www.joemaller.com/
-Description: Version everything with npm.
-Author: Joe Maller
-Version: 1.0.1-2
-Author URI: http://www.github.com/joemaller
-*/
